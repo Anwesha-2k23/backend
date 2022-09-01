6ac2005b4d142ce5b63283b42c023853bc08cdbc
raunak-dev-edu <raunak_2101cb47@iitp.ac.in>
2022-09-01 23:37:06 +0530

    events model update

--- anwesha/event/models.py
+++ anwesha/event/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 
-class Events(models.Model){
+class Events(models.Model) :
 
     id                    = models.CharField(unique=True, max_length=10);  
-    name                  = models.CharField(max_length=100);
-    organizer             = models.CharField(max_length=100);
-    venue                 = models.CharField(max_length=255);
-    date                  = models.dateField();
-    start_time            = models.DateTimefield();
-    description           = models.TextField();
-    end_time              = models.DateTimeField();
-    prize                 = models.CharField(max_length=150);
-    registration_fee      = models.DecimalField(max_digits=8, decimal_places=2);
-    video                 = models.URLField();
-    max_team_size         = models.SmallIntegerField(max_length=);
-    registration_deadline = models.DateTimeField();
-    poster                = models.URLField();
-}
+    name                  = models.CharField(max_length=100)
+    organizer             = models.CharField(max_length=100)
+    venue                 = models.CharField(max_length=255)
+    date                  = models.dateField()
+    start_time            = models.DateTimefield()
+    description           = models.TextField()
+    end_time              = models.DateTimeField()
+    prize                 = models.CharField(max_length=150)
+    registration_fee      = models.DecimalField(max_digits=8, decimal_places=2)
+    video                 = models.URLField()
+    max_team_size         = models.SmallIntegerField()
+    registration_deadline = models.DateTimeField()
+    poster                = models.URLField()
