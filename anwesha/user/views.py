5b9f2ab8a8edfc1aa59838b464bd576116a9da95
RupakBiswas-2304 <rupak_2001cs57@iitp.ac.in>
2022-09-09 00:33:29 +0530

    sample api added

--- anwesha/user/views.py
+++ anwesha/user/views.py
@@ -4,12 +4,12 @@ from django.http import JsonResponse
 from .models import User
 # Create your views here.
 
-# def alluser(request):
-#     if request.method == 'GET':
-#         users = User.objects.all()
-#         users = list(users.values())
-#         return JsonResponse(users, safe=False)
-#     else:
-#         response = JsonResponse({'message': 'Hello, World!'})
-#         return response
+def alluser(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        users = list(users.values())
+        return JsonResponse(users, safe=False)
+    else:
+        response = JsonResponse({'message': 'Hello, World!'})
+        return response
 
