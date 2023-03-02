f343ac76b3b2ab19e686db28edaa913b48959f7d
Ubuntu <ubuntu@ip-172-31-41-98.ap-south-1.compute.internal>
2023-03-02 06:23:45 +0000

    minor fixes

--- anwesha/anwesha/settings.py
+++ anwesha/anwesha/settings.py
@@ -102,24 +102,24 @@ WSGI_APPLICATION = "anwesha.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if CONFIGURATION == 'production':
+if CONFIGURATION == 'local':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-#elif CONFIGURATION == 'production':
-#   DATABASES = {
-#        "default": {
-#           "ENGINE": "django.db.backends.mysql",
-#            "NAME": env("DB_NAME"),
-#            "USER": env("DB_USER"),
-#            "PASSWORD": env("DB_PASSWORD"),
-#            "HOST": env("DB_HOST"),
-#            "PORT": env("DB_PORT"),
-#        } 
-#    }
+elif CONFIGURATION == 'production':
+   DATABASES = {
+        "default": {
+           "ENGINE": "django.db.backends.mysql",
+            "NAME": env("DB_NAME"),
+            "USER": env("DB_USER"),
+            "PASSWORD": env("DB_PASSWORD"),
+            "HOST": env("DB_HOST"),
+            "PORT": env("DB_PORT"),
+        } 
+    }
 
 
 AUTHENTICATION_BACKENDS = [
