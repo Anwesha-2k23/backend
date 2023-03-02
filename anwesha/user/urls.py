a60136efca817b099ba74ede6925639c793554d5
Rupak Biswas <rupak_2001cs57@iitp.ac.in>
2023-03-02 12:03:26 +0530

    forgot password

--- anwesha/user/urls.py
+++ anwesha/user/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views  import LogOut,  Login, editProfile, register , forgetPassword, verifyEmail , sendVerificationEmail,Oauth_Login, Oauth_Logout
+from .views  import LogOut,  Login, editProfile, register , ForgetPassword, verifyEmail , sendVerificationEmail,Oauth_Login, Oauth_Logout
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import views as auth_views
 
@@ -9,7 +9,7 @@ user_urls = [
     path('logout', LogOut.as_view() , name='logout'),
     path('editprofile' , editProfile.as_view() , name = 'editProfile'),
     path('register', register.as_view(), name='register'),
-    path('forgetpassword/<str:forget_password_user>', forgetPassword.as_view() ,  name='forget_password_user'), # an upredicated 
+    path('forgetpassword/<str:forget_password_user>', ForgetPassword.as_view() ,  name='forget_password_user'), # an upredicated 
     path('verifyemail' , sendVerificationEmail.as_view(), name='send_verificaion_email'),
     path('verifyemail/<str:pk>' , verifyEmail , name='verifyEmail' ),
     path('oauth-login/',Oauth_Login.as_view(),name='oauth-login'),
