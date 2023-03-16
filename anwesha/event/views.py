e7aacde21e87409d1b34e0dcd17ca2906dbeee82
Tejax-v2 <tejastupke74@gmail.com>
2023-03-16 22:17:15 +0530

    checkeventregistration api updated

--- anwesha/event/views.py
+++ anwesha/event/views.py
@@ -111,18 +111,21 @@ class Check_Event_Registration(APIView):
             signature = request.data['signature']
             print(signature)
             event_id = request.data['event_id']
-            user = User.objects.get(signature=signature)
+            try:
+                user = User.objects.get(signature=signature)
+            except:
+                return JsonResponse({"message": "Invalid signature"},status=400)
             event = Events.objects.get(id=event_id)
             if event.max_team_size == 1 and event.min_team_size == 1:
                 if SoloParicipants.objects.filter(event_id=event_id, anwesha_id=user.anwesha_id, payment_done = True).exists():
-                    return JsonResponse({"anwesha_id":user.anwesha_id,"message": "User is Registered" , "status": '200'},status=200)
+                    return JsonResponse({"anwesha_id":user.anwesha_id,"username":user.full_name,"message": "User is Registered","entry_status":False},status=200)
                 else:
-                    return JsonResponse({"anwesha_id":user.anwesha_id,"message": "User is not Registered" , "status": '200'},status=200)
+                    return JsonResponse({"anwesha_id":user.anwesha_id,"username":user.full_name,"message": "User is not Registered"},status=401)
             else:
                 if Team.objects.filter(event_id=event_id,leader_id=user.anwesha_id, payment_done = True).exists():
-                    return JsonResponse({"anwesha_id":user.anwesha_id,"message": "Team is Registered" , "status": '200'},status=200)
+                    return JsonResponse({"anwesha_id":user.anwesha_id,"username":user.full_name,"message": "Team is Registered","entry_status": False},status=200)
                 else:
-                    return JsonResponse({"anwesha_id":user.anwesha_id,"message": "Team is not Registered" , "status": '200'},status=200)
+                    return JsonResponse({"anwesha_id":user.anwesha_id,"username":user.full_name,"message": "Team is not Registered"},status=401)
         except:
             return JsonResponse({"message": "Invalid method" , "status": '405'},status=405)
 
